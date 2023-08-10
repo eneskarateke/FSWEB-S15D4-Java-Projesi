82d68a70983508bdb7b786ad0431e0a3b23241e5
eneskarateke <eneskarateke@gmail.com>
2023-08-10 21:50:35 +0300

    check for palindrome fifo  done

--- src/com/workintech/Main.java
+++ src/com/workintech/Main.java
@@ -5,16 +5,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(checkForPalindrome("abccba"));
-        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
-        System.out.println(checkForPalindrome("I did, did I?"));
-        System.out.println(checkForPalindrome("hello"));
-        System.out.println(checkForPalindrome("Don't node"));
+//        System.out.println(checkForPalindrome("abccba"));
+//        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+//        System.out.println(checkForPalindrome("I did, did I?"));
+//        System.out.println(checkForPalindrome("hello"));
+//        System.out.println(checkForPalindrome("Don't node"));
+
+        System.out.println("**************************************");
+
+        System.out.println(checkForPalindromeFIFO("abccba"));
+        System.out.println(checkForPalindromeFIFO("Was it a car or a cat I saw ?"));
+        System.out.println(checkForPalindromeFIFO("I did, did I?"));
+        System.out.println(checkForPalindromeFIFO("hello"));
+        System.out.println(checkForPalindromeFIFO("Don't node"));
+        System.out.println(checkForPalindromeFIFO("RACECAR"));
+
 
     }
 
     public static boolean checkForPalindrome(String text) {
-        LinkedList<Character> stack = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
 
 
         String noPuncText = text
@@ -23,25 +33,23 @@ public class Main {
                 .replaceAll("'","")
                 .replaceAll(" ","");
         String lowerCase= noPuncText.toLowerCase(Locale.forLanguageTag("en-EN"));
-//        System.out.println("lowercase: " + lowerCase);
+        System.out.println("lowercase: " + lowerCase);
 
         for (int i = 0; i <lowerCase.length() ; i++) {
             char c = lowerCase.charAt(i);
-            stack.push(c);
+            stack.add(c);
         }
-//        System.out.println("stack: " + stack);
+        System.out.println("stack: " + stack);
 
 
         String stackString = "";
 
-        // using iterator for traversing a linkedlist
         Iterator<Character> iterator = stack.iterator();
 
         while (iterator.hasNext()) {
-            // appending using "+" operator
-            stackString = stackString + iterator.next();
+            stackString = iterator.next() + stackString;;
         }
-//        System.out.println("stackString: " + stackString);
+        System.out.println("stackString: " + stackString);
 
 
 
@@ -54,4 +62,34 @@ return false;
 
 
     }
+
+    public static boolean checkForPalindromeFIFO(String text) {
+
+        LinkedList<Character> stack = new LinkedList<>();
+        LinkedList<Character> queue = new LinkedList<>();
+        String lowerCase = text.toLowerCase(Locale.ENGLISH);
+
+
+        for(int i = 0; i < lowerCase.length(); i++){
+            char c = lowerCase.charAt(i);
+            if(c >= 'a' && c <= 'z'){
+                queue.addLast(c);
+                stack.push(c);
+            }
+        }
+//        System.out.println("queue: " + queue);
+//        System.out.println("stack: " + stack);
+
+
+        if(stack.equals(queue)){
+            System.out.println("EQUAL");
+            return true;
+        } else{
+            System.out.println("NOT EQUAL");
+            return false;
+        }
+
+    }
+
+
 }
